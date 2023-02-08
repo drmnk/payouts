91061d72177e4ef9d90533561d54a72009fe7665
Denis Romanenko <deromanenko@gmail.com>
2023-02-08 17:29:32 +0300

    feat: select with status value in table

--- resources/js/Pages/Payouts.vue
+++ resources/js/Pages/Payouts.vue
@@ -1,26 +1,38 @@
-<template>
-    <v-table>
-        <thead>
-            <tr>
-                <th>#ID</th>
-                <th>Логин</th>
-                <th>Реквизиты</th>
-                <th>Сумма</th>
-                <th>Валюта</th>
-                <th>Статус</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="payout in payouts" :key="payout.id">
-                <td>{{ payout.id }}</td>
-                <td>{{ payout.username }}</td>
-                <td>Показать реквизиты</td>
-                <td>{{ payout.amount }}</td>
-                <td>{{ payout.currency }}</td>
-                <td>{{ payout.status }}</td>
-            </tr>
-        </tbody>
-    </v-table>
+<template class="tw-bg-amber-50">
+    <!-- Container-->
+        <div class="tw-m-10 tw-rounded-xl tw-shadow-xl">
+            <v-table>
+            <thead class="tw-bg-slate-300">
+                <tr class="tw-text-xl">
+                    <th>#ID</th>
+                    <th>Логин</th>
+                    <th>Реквизиты</th>
+                    <th>Сумма</th>
+                    <th>Валюта</th>
+                    <th>Статус</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="payout in payouts" :key="payout.id">
+                    <td>{{ payout.id }}</td>
+                    <td>{{ payout.username }}</td>
+                    <td>
+                        <a href="#" class="tw-text-blue-500 tw-underline">Показать реквизиты</a>
+                    </td>
+                    <td>{{ payout.amount }}</td>
+                    <td>{{ payout.currency }}</td>
+                    <td>
+                        <v-select class="tw-mt-4"
+                            v-model="payout.status"
+                            :items="['Не оплачен', 'Оплачен']"
+                            variant="solo"
+                        >
+                        </v-select>
+                    </td>
+                </tr>
+            </tbody>
+        </v-table>
+        </div>
 </template>
 
 <script setup>
