46c7e093a9a1315c2c3cea9ebbcc841322502188
Denis Romanenko <deromanenko@gmail.com>
2023-02-08 16:03:03 +0300

    feat: factory for mass-creating of Payouts

---
+++ database/factories/PayoutFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PayoutFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'username' => fake()->userName(),
+            'details' => fake()->text(),
+            'amount' => fake()->randomFloat(2),
+            'currency' => fake()->currencyCode(),
+        ];
+    }
+}
